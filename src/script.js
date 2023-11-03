bf8ce1bf4f7fdbab26c09f2e0c1c3edfc219b38c
chrisweber <cweber@firehydrant.io>
2023-11-03 12:36:45 -0400

    Restructured directories, added top-level index and styling

--- src/script.js
+++ src/script.js
@@ -1,37 +1,3 @@
-document.getElementById('bulkUpdateForm').addEventListener('submit', async (e) => {
-    e.preventDefault();
-
-    const authToken = document.getElementById('authToken').value;
-    const autoAlert = document.getElementById('autoAlert').value === "true";
-    const autoAdd = document.getElementById('autoAdd').value === "true";
-
-    try {
-        const response = await fetch('https://5775d63d1acd.ngrok.app/update-services', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ authToken, autoAlert, autoAdd })
-        });
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const result = await response.json();
-
-        if (result.success) {
-            alert('Services updated successfully!');
-        } else {
-            alert('Error updating services: ' + result.error);
-        }
-    } catch (error) {
-        console.error('Error while sending request to backend:', error);
-        alert('Failed to update services. Check the console for more information.');
-    }
+document.addEventListener('DOMContentLoaded', () => {
+    // Future functionality can go here
 });
-
-function toggleExpandableFields() {
-    const fields = document.querySelector('.expandable-fields');
-    fields.style.display = fields.style.display === 'none' ? 'block' : 'none';
-}

07e07b1768f51a87faa0e3fe093e7c261b591b1a
chrisweber <cweber@firehydrant.io>
2023-11-03 13:41:47 -0400

    updated us file

--- functions/updateServices.js
+++ functions/updateServices.js
@@ -1,48 +1,42 @@
-// Import axios for making HTTP requests
 const axios = require('axios');
 
 exports.handler = async (event) => {
-  // Only allow POST
   if (event.httpMethod !== 'POST') {
-    return {
-      statusCode: 405,
-      body: JSON.stringify({ message: 'Method Not Allowed' })
-    };
+    return { statusCode: 405, body: JSON.stringify({ message: 'Method Not Allowed' }) };
   }
 
   const { authToken, autoAlert, autoAdd } = JSON.parse(event.body);
+  const apiEndpoint = process.env.FIREHYDRANT_API_ENDPOINT;
+  const bearerToken = `Bearer ${authToken || process.env.FIREHYDRANT_API_TOKEN}`;
 
   try {
-    // Get all services
-    const servicesResponse = await axios.get('https://api.firehydrant.io/v1/services', {
-      headers: { Authorization: `Bearer ${authToken}` }
+    const servicesResponse = await axios.get(`${apiEndpoint}/v1/services`, {
+      headers: { Authorization: bearerToken }
     });
 
-    // Loop through services and update them
-    const updatePromises = servicesResponse.data.map((service) => {
-      return axios.patch(`https://api.firehydrant.io/v1/services/${service.id}`, {
+    const updatePromises = servicesResponse.data.map(service =>
+      axios.patch(`${apiEndpoint}/v1/services/${service.id}`, {
         alert_on_add: autoAlert,
         auto_add_responding_team: autoAdd
       }, {
-        headers: { Authorization: `Bearer ${authToken}` }
-      });
-    });
+        headers: { Authorization: bearerToken }
+      })
+    );
 
-    // Wait for all updates to complete
-    await Promise.all(updatePromises);
+    const results = await Promise.allSettled(updatePromises);
+    const success = results.filter(result => result.status === 'fulfilled');
 
-    // Return a success response
     return {
       statusCode: 200,
-      body: JSON.stringify({ success: true })
+      body: JSON.stringify({ success: true, updated: success.length })
     };
   } catch (error) {
-    console.error('Error updating services:', error);
+    const status = error.response?.status || 500;
+    const message = error.response?.statusText || error.message;
 
-    // Return an error response
     return {
-      statusCode: error.response?.status || 500,
-      body: JSON.stringify({ success: false, error: error.message })
+      statusCode: status,
+      body: JSON.stringify({ success: false, error: message })
     };
   }
 };

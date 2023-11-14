7129f27103eeeaf678a21512f3c2edcf88e4f8af
chrisweber <cweber@firehydrant.io>
2023-11-14 14:51:10 -0500

    added bulk add services

--- src/bulk_add_services/script.js
+++ src/bulk_add_services/script.js
@@ -1,4 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+        fetch('../sidebar.html') // Make sure the path to sidebar.html is correct
+          .then(response => {
+            if (!response.ok) {
+              throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.text();
+          })
+          .then(data => {
+            document.getElementById('sidebar-placeholder').innerHTML = data;
+            // Ensure the form is present in the sidebar HTML
+            // Then attach the event listener to the form
+            attachFormSubmitListener();
+          })
+          .catch(error => console.error('Error loading the sidebar:', error));
+
     const form = document.getElementById('bulkServiceForm');
     const addButton = document.getElementById('addServiceButton');
     const container = document.getElementById('serviceFieldsContainer');

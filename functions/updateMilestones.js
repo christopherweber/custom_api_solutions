e052f4f23e14fc7d58d0755d737b4d46897448ae
chrisweber <cweber@firehydrant.io>
2023-11-17 17:57:55 -0500

    added the update milestone functions serverless

--- functions/updateMilestones.js
+++ functions/updateMilestones.js
@@ -7,14 +7,15 @@ exports.handler = async (event) => {
 
     try {
         const { authToken, startingMilestone, targetMilestone } = JSON.parse(event.body);
-        // Validate the input
+
         if (!authToken || !startingMilestone || !targetMilestone) {
             return { statusCode: 400, body: 'Missing required fields' };
         }
 
-        // Here you can implement the logic to update the milestones
-        // For example, call a function to fetch incidents and update their milestones
-        // This part will be similar to the script.js logic, but modified for server-side execution
+        console.log(`Updating from ${startingMilestone} to ${targetMilestone} with token: ${authToken}`);
+
+        const apiResponse = await yourApiRequestFunction();
+        console.log('API Response:', apiResponse);
 
         return {
             statusCode: 200,

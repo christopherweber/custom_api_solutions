8f30a5e693c3649e27aa67d3e44352d7579dd9fa
chrisweber <cweber@firehydrant.io>
2023-11-19 13:57:36 -0500

    added the update milestone complete rework on update js file and script v2

--- src/bulk_update_milestones/script.js
+++ src/bulk_update_milestones/script.js
@@ -26,14 +26,24 @@ function populateMilestoneOptions() {
         "postmortem_started",
         "postmortem_completed"
     ];
+    const capitalize = (s) => s.charAt(0).toUpperCase() + s.slice(1);
 
     const startingMilestoneSelect = document.getElementById('startingMilestone');
     const targetMilestoneSelect = document.getElementById('targetMilestone');
 
     milestonesSequence.forEach(milestone => {
-        startingMilestoneSelect.options.add(new Option(milestone, milestone));
-        targetMilestoneSelect.options.add(new Option(milestone, milestone));
+        startingMilestoneSelect.options.add(new Option(capitalize(milestone), milestone));
+        targetMilestoneSelect.options.add(new Option(capitalize(milestone), milestone));
     });
+}
+
+function showLoadingMessage() {
+    document.getElementById('loadingMessage').textContent = 'Working on it!';
+}
+
+function showSuccessMessage() {
+    document.getElementById('loadingMessage').textContent = '';
+    document.getElementById('successMessage').textContent = 'All incidents have been processed.';
 }
 
 function attachFormSubmitListener() {
@@ -41,10 +51,16 @@ function attachFormSubmitListener() {
     form.addEventListener('submit', function(event) {
         event.preventDefault();
 
+        const confirmed = window.confirm("This will move ALL incidents with this milestone. Are you sure you want to process?");
+        if (!confirmed) {
+            return; // Stop the function if the user does not confirm
+        }
+
         const authToken = document.getElementById('authToken').value;
         const startingMilestone = document.getElementById('startingMilestone').value;
         const targetMilestone = document.getElementById('targetMilestone').value;
 
+        showLoadingMessage();
         updateMilestones(authToken, startingMilestone, targetMilestone);
     });
 }
@@ -63,10 +79,10 @@ function updateMilestones(authToken, startingMilestone, targetMilestone) {
     })
     .then(data => {
         console.log(data.message);
-        // Handle successful response
+        showSuccessMessage();
     })
     .catch(error => {
         console.error('Error:', error);
-        // Handle error response
+        document.getElementById('loadingMessage').textContent = ''; 
     });
 }

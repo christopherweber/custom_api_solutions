d86a7963247ed057271094f13964a7f4485b2013
chrisweber <cweber@firehydrant.io>
2023-11-21 14:46:01 -0500

    complete change of api

--- functions/getAnalytics.js
+++ functions/getAnalytics.js
@@ -3,34 +3,51 @@ const { parse } = require('json2csv');
 
 exports.handler = async function(event) {
   try {
+    // Parse the request body to get the parameters
     const { authToken, startDate, endDate } = JSON.parse(event.body);
+
+    // Construct the URL for the FireHydrant API request
     const incidentsUrl = `https://api.firehydrant.io/v1/incidents?start_date=${startDate}&end_date=${endDate}`;
-    console.log("in analytics.js")
-    console.log("here is the URL: " + incidentsUrl)
-    console.log("API Response:", incidentsResponse.data);   
-    console.log("here is the incidents from getAnalytics.js " + incidents)
+    console.log("API Request URL: " + incidentsUrl);
 
-    const incidentsResponse = await axios.get(incidentsUrl, { headers: { 'Authorization': authToken } });
-    const incidents = incidentsResponse.data;
+    // Fetch incidents data from the FireHydrant API
+    const response = await axios.get(incidentsUrl, {
+      headers: { 'Authorization': authToken }
+    });
 
-    // Convert to CSV
-    const fields = ['id', 'name', 'created_at', 'started_at', 'discarded_at', 'summary', 'customer_impact_summary', 'description', 'current_milestone', 'number', 'priority', 'severity', /* Add other fields as necessary */];
+    // Check if the incidents data is in the expected format
+    if (!response.data || !Array.isArray(response.data)) {
+      throw new Error("Unexpected response format from the FireHydrant API");
+    }
+
+    // Extract the incidents data from the response
+    const incidents = response.data;
+
+    // Convert the incidents data to CSV format
+    const fields = ['id', 'name', 'created_at', 'started_at', 'discarded_at', 'summary', 'customer_impact_summary', 'description', 'current_milestone', 'number', 'priority', 'severity'];
     const csv = parse(incidents, { fields });
 
-    console.log(JSON.stringify({ incidents, csv })); // Add this line in getAnalytics.js before the return statement
+    // Log the incidents data and CSV for debugging
+    console.log("Incidents Data:", incidents);
+    console.log("CSV Data:", csv);
+
+    // Return the incidents data and CSV in the response
     return {
       statusCode: 200,
       body: JSON.stringify({ incidents, csv })
     };
   } catch (error) {
+    // Log and return the error
     console.error('Error:', error);
     return {
-
       statusCode: 500,
       body: JSON.stringify({ error: error.message })
     };
   }
 };
+
+// Additional functions (if needed) can be added below
+
 
 async function fetchIncidentDetails(incidentId, authToken) {
   // Nothing here yet 

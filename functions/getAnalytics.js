d41d5d344768d0538d0ee0da02cb2116f00262c6
chrisweber <cweber@firehydrant.io>
2023-11-21 12:23:54 -0500

    complete change of api

---
+++ functions/getAnalytics.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+const { parse } = require('json2csv');
+
+exports.handler = async function(event) {
+  try {
+    const { authToken, startDate, endDate } = JSON.parse(event.body);
+    const incidentsUrl = `https://api.firehydrant.io/v1/incidents?start_date=${startDate}&end_date=${endDate}`;
+
+    // Fetch incidents
+    const incidentsResponse = await axios.get(incidentsUrl, { headers: { 'Authorization': authToken } });
+    const incidents = incidentsResponse.data.incidents;  // Assuming the API returns an object with an 'incidents' array
+
+    // Convert to CSV
+    const fields = ['id', 'name', 'created_at', 'started_at', 'discarded_at', 'summary', 'customer_impact_summary', 'description', 'current_milestone', 'number', 'priority', 'severity', /* Add other fields as necessary */];
+    const csv = parse(incidents, { fields });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ incidents, csv })
+    };
+  } catch (error) {
+    console.error('Error:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message })
+    };
+  }
+};
+
+async function fetchIncidentDetails(incidentId, authToken) {
+  // Nothing here yet
+}
--- src/custom_analytics/script.js
+++ src/custom_analytics/script.js
@@ -14,35 +14,28 @@ function attachFormSubmitListener() {
         event.preventDefault();
 
         const authToken = document.getElementById('authToken').value;
-        const by = document.getElementById('by').value;
-        const bucketSize = document.getElementById('bucketSize').value;
         const startDate = document.getElementById('startDate').value;
         const endDate = document.getElementById('endDate').value;
 
         showLoadingMessage();
-        fetchAnalyticsData(authToken, by, bucketSize, startDate, endDate);
+        fetchAnalyticsData(authToken, startDate, endDate);
     });
 }
 
-function fetchAnalyticsData(authToken, by, bucketSize, startDate, endDate) {
-    // Show the loading message
+function fetchAnalyticsData(authToken, startDate, endDate) {
     showLoadingMessage();
 
-    // Make a POST request to the Netlify function
-    fetch('/.netlify/functions/getMetrics', {
+    fetch('/.netlify/functions/getAnalytics', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ authToken, by, bucketSize, startDate, endDate })
+        body: JSON.stringify({ authToken, startDate, endDate })
     })
     .then(response => response.json())
     .then(data => {
-        // Handle successful response
-        // Hide the loading message
         hideLoadingMessage();
         displayReportResults(data);
     })
     .catch(error => {
-        // Handle any errors
         console.error('Error:', error);
         hideLoadingMessage();
     });
@@ -60,35 +53,29 @@ function hideLoadingMessage() {
 
 function displayReportResults(data) {
     const reportResultsElement = document.getElementById('reportResults');
-    reportResultsElement.innerHTML = ''; // Clear previous results
+    reportResultsElement.innerHTML = '';
 
-    // Check if there's data to display
-    if (!data || data.buckets.length === 0) {
+    if (!data || data.incidents.length === 0) {
         reportResultsElement.textContent = 'No data to display.';
         return;
     }
 
-    // Process data into a flat structure for display
-    let flattenedData = [];
-    data.buckets.forEach(bucket => {
-        Object.entries(bucket.metrics).forEach(([key, value]) => {
-            if (data.display_information[key]) {
-                flattenedData.push({
-                    DateRange: `${bucket.starts_at} to ${bucket.ends_at}`,
-                    Severity: data.display_information[key].name,
-                    Count: value
-                });
-            }
-        });
-    });
-
     // Create and populate table
+    const table = createTable(data.incidents);
+    reportResultsElement.appendChild(table);
+
+    // Create CSV download link
+    const csvDownloadLink = createCSVDownloadLink(data.csv);
+    reportResultsElement.appendChild(csvDownloadLink);
+}
+
+function createTable(incidents) {
     const table = document.createElement('table');
     const thead = document.createElement('thead');
     const tbody = document.createElement('tbody');
-    
-    // Create headers
-    const headers = Object.keys(flattenedData[0]);
+
+    // Create headers based on incident fields
+    const headers = ['id', 'name', 'created_at', 'started_at', 'discarded_at', 'summary', 'customer_impact_summary', 'description', 'current_milestone', 'number', 'priority', 'severity', /* Add other headers as necessary */];
     const headerRow = document.createElement('tr');
     headers.forEach(headerText => {
         const header = document.createElement('th');
@@ -98,11 +85,11 @@ function displayReportResults(data) {
     thead.appendChild(headerRow);
 
     // Create rows
-    flattenedData.forEach(item => {
+    incidents.forEach(incident => {
         const row = document.createElement('tr');
-        Object.values(item).forEach(text => {
+        headers.forEach(header => {
             const cell = document.createElement('td');
-            cell.textContent = text;
+            cell.textContent = incident[header] || ''; // Handle null or undefined values
             row.appendChild(cell);
         });
         tbody.appendChild(row);
@@ -110,7 +97,13 @@ function displayReportResults(data) {
 
     table.appendChild(thead);
     table.appendChild(tbody);
-    reportResultsElement.appendChild(table);
+    return table;
 }
 
-
+function createCSVDownloadLink(csvData) {
+    const link = document.createElement('a');
+    link.href = 'data:text/csv;charset=utf-8,' + encodeURIComponent(csvData);
+    link.download = 'incidents_report.csv';
+    link.textContent = 'Download CSV';
+    return link;
+}

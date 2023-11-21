a752e9f075fa37c288361e019b0221bca659e575
chrisweber <cweber@firehydrant.io>
2023-11-21 10:48:51 -0500

    metric debugger

--- functions/getMetrics.js
+++ functions/getMetrics.js
@@ -3,47 +3,37 @@ const { parse } = require('json2csv');
 
 exports.handler = async function(event, context) {
   try {
-    // Log the event to see if it's receiving the correct data
-    console.log('Event:', event);
-    
     const { authToken, bucketSize, by, startDate, endDate } = JSON.parse(event.body);
 
-    // Log the parsed body to make sure it contains the right parameters
-    console.log('Parsed body:', { authToken, bucketSize, by, startDate, endDate });
+    // Build the params object dynamically
+    let params = {
+      bucket_size: bucketSize,
+      by: by,
+      end_date: endDate,
+      start_date: startDate
+    };
+
+    // If conditions are not needed, they should not be included in the params
+    // if (conditions) {
+    //   params.conditions = conditions; // Only include conditions if it's provided
+    // }
 
     const response = await axios.get('https://api.firehydrant.io/v1/metrics/incidents', {
       headers: {
         'Authorization': authToken
       },
-      params: {
-        bucket_size: bucketSize,
-        by: by,
-        conditions: '', // Ensure this is the intended usage
-        end_date: endDate,
-        start_date: startDate
-      }
+      params: params // Use the params object
     });
 
-    // Log the response status and data
-    console.log('Response status:', response.status);
-    console.log('Response data:', response.data);
-
+    // Convert the data to CSV
     const csv = parse(response.data);
-
-    // Log the CSV to see if it's formatted correctly
-    console.log('CSV:', csv);
 
     return {
       statusCode: 200,
       body: JSON.stringify({ csv })
     };
   } catch (error) {
-    // Log the error to see what went wrong
     console.error('Error:', error);
-
-    // It's helpful to log the error message and stack for detailed info
-    console.error('Error message:', error.message);
-    console.error('Error stack:', error.stack);
 
     return {
       statusCode: 500,

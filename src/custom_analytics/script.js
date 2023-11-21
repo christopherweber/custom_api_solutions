2cb9012c83bdfd2b23c2dccd12b28a890bfa4352
chrisweber <cweber@firehydrant.io>
2023-11-21 10:37:18 -0500

    added analytics dashboard display

--- src/custom_analytics/script.js
+++ src/custom_analytics/script.js
@@ -1,50 +1,100 @@
-document.getElementById('parameters-form').addEventListener('submit', function(event) {
-    event.preventDefault();
+document.addEventListener('DOMContentLoaded', () => {
     fetch('../sidebar.html')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.text();
-    })
-    .then(data => {
-        document.getElementById('sidebar-placeholder').innerHTML = data;
-        populateMilestoneOptions();
-        attachFormSubmitListener();
-    })
-    .catch(error => console.error('Error loading the sidebar:', error));
-    
-    const authToken = document.getElementById('auth-token').value;
-    const bucketSize = document.getElementById('bucket-size').value;
-    const by = document.getElementById('by').value;
-    const startDate = document.getElementById('start-date').value;
-    const endDate = document.getElementById('end-date').value;
-  
-    // Trigger the Netlify function
-    fetchData(authToken, bucketSize, by, startDate, endDate);
-  });
-  
-  function fetchData(authToken, bucketSize, by, startDate, endDate) {
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('sidebar-placeholder').innerHTML = data;
+            attachFormSubmitListener();
+        })
+        .catch(error => console.error('Error loading the sidebar:', error));
+});
+
+function attachFormSubmitListener() {
+    const form = document.getElementById('analyticsForm');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const authToken = document.getElementById('authToken').value;
+        const by = document.getElementById('by').value;
+        const bucketSize = document.getElementById('bucketSize').value;
+        const startDate = document.getElementById('startDate').value;
+        const endDate = document.getElementById('endDate').value;
+
+        showLoadingMessage();
+        fetchAnalyticsData(authToken, by, bucketSize, startDate, endDate);
+    });
+}
+
+function fetchAnalyticsData(authToken, by, bucketSize, startDate, endDate) {
+    // Show the loading message
+    showLoadingMessage();
+
+    // Make a POST request to the Netlify function
     fetch('/.netlify/functions/getMetrics', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ authToken, bucketSize, by, startDate, endDate })
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ authToken, by, bucketSize, startDate, endDate })
     })
     .then(response => response.json())
     .then(data => {
-      displayData(data);
-      document.getElementById('download-csv').style.display = 'block';
+        // Handle successful response
+        // Hide the loading message
+        hideLoadingMessage();
+        displayReportResults(data);
     })
-    .catch(error => console.error('Error:', error));
-  }
-  
-  function displayData(data) {
-    // Code to display data on the page
-  }
-  
-  document.getElementById('download-csv').addEventListener('click', function() {
-    // Code to handle CSV download
-  });
-  
+    .catch(error => {
+        // Handle any errors
+        console.error('Error:', error);
+        hideLoadingMessage();
+    });
+}
+
+function showLoadingMessage() {
+    const loadingElement = document.getElementById('loadingMessage');
+    loadingElement.textContent = 'Loading... Please wait.';
+    loadingElement.style.display = 'block';
+}
+
+function hideLoadingMessage() {
+    document.getElementById('loadingMessage').style.display = 'none';
+}
+
+function displayReportResults(data) {
+    const reportResultsElement = document.getElementById('reportResults');
+    reportResultsElement.innerHTML = ''; // Clear previous results
+
+    // Create a table element
+    const table = document.createElement('table');
+    const thead = document.createElement('thead');
+    const tbody = document.createElement('tbody');
+
+    // Assuming data is an array of objects
+    if (data.length > 0) {
+        // Create headers
+        const headers = Object.keys(data[0]);
+        const headerRow = document.createElement('tr');
+        headers.forEach(headerText => {
+            const header = document.createElement('th');
+            header.textContent = headerText;
+            headerRow.appendChild(header);
+        });
+        thead.appendChild(headerRow);
+
+        // Create rows
+        data.forEach(item => {
+            const row = document.createElement('tr');
+            Object.values(item).forEach(text => {
+                const cell = document.createElement('td');
+                cell.textContent = text;
+                row.appendChild(cell);
+            });
+            tbody.appendChild(row);
+        });
+    } else {
+        reportResultsElement.textContent = 'No data to display.';
+    }
+
+    table.appendChild(thead);
+    table.appendChild(tbody);
+    reportResultsElement.appendChild(table);
+}
+

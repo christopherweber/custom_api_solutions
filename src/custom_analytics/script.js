c57e11d84e37a3cf0f9f4b5d5e002956d253ad7e
chrisweber <cweber@firehydrant.io>
2023-11-21 15:03:49 -0500

    complete change of api 2

--- src/custom_analytics/script.js
+++ src/custom_analytics/script.js
@@ -12,11 +12,9 @@ function attachFormSubmitListener() {
     const form = document.getElementById('analyticsForm');
     form.addEventListener('submit', function(event) {
         event.preventDefault();
-
         const authToken = document.getElementById('authToken').value;
         const startDate = document.getElementById('startDate').value;
         const endDate = document.getElementById('endDate').value;
-
         showLoadingMessage();
         fetchAnalyticsData(authToken, startDate, endDate);
     });
@@ -24,7 +22,6 @@ function attachFormSubmitListener() {
 
 function fetchAnalyticsData(authToken, startDate, endDate) {
     showLoadingMessage();
-
     fetch('/.netlify/functions/getAnalytics', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -32,7 +29,6 @@ function fetchAnalyticsData(authToken, startDate, endDate) {
     })
     .then(response => response.json())
     .then(data => {
-        console.log("Received data:", data); // Debugging line
         hideLoadingMessage();
         displayReportResults(data);
     })
@@ -41,7 +37,6 @@ function fetchAnalyticsData(authToken, startDate, endDate) {
         hideLoadingMessage();
     });
 }
-
 
 function showLoadingMessage() {
     const loadingElement = document.getElementById('loadingMessage');
@@ -57,36 +52,22 @@ function displayReportResults(data) {
     const reportResultsElement = document.getElementById('reportResults');
     reportResultsElement.innerHTML = '';
 
-    // Check if the data is in the expected format
-    if (!data || !data.data || data.data.length === 0) {
+    if (!data || !Array.isArray(data.data) || data.data.length === 0) {
         reportResultsElement.textContent = 'No data to display.';
         return;
     }
 
-    // Assuming data is wrapped in a "data" key as per your example
-    const incidents = data.data;
-    console.log("incidents: " + incidents)
-
-    // Extract and display only the IDs
-    const ids = incidents.map(incident => incident.id);
-    const list = document.createElement('ul');
-    ids.forEach(id => {
-        const listItem = document.createElement('li');
-        listItem.textContent = id;
-        list.appendChild(listItem);
-    });
-
-    reportResultsElement.appendChild(list);
+    const table = createTable(data.data);
+    reportResultsElement.appendChild(table);
 }
-
 
 function createTable(incidents) {
     const table = document.createElement('table');
+    table.setAttribute('border', '1');
     const thead = document.createElement('thead');
     const tbody = document.createElement('tbody');
 
-    // Create headers based on keys of the first incident
-    const headers = Object.keys(incidents[0]);
+    const headers = ['id', 'name', 'created_at', 'started_at', 'discarded_at', 'summary', 'customer_impact_summary', 'description', 'current_milestone', 'number', 'priority', 'severity'];
     const headerRow = document.createElement('tr');
     headers.forEach(headerText => {
         const header = document.createElement('th');
@@ -94,64 +75,18 @@ function createTable(incidents) {
         headerRow.appendChild(header);
     });
     thead.appendChild(headerRow);
+    table.appendChild(thead);
 
-    // Create rows for each incident
     incidents.forEach(incident => {
         const row = document.createElement('tr');
         headers.forEach(header => {
             const cell = document.createElement('td');
-            if (header in incident) {
-                // Check if the value is an object and convert it to string
-                cell.textContent = typeof incident[header] === 'object' ? JSON.stringify(incident[header]) : incident[header];
-            } else {
-                cell.textContent = '';
-            }
+            cell.textContent = incident[header] || '';
             row.appendChild(cell);
         });
         tbody.appendChild(row);
     });
 
-    table.appendChild(thead);
     table.appendChild(tbody);
     return table;
-}
-
-
-function createTable(incidents) {
-    const table = document.createElement('table');
-    const thead = document.createElement('thead');
-    const tbody = document.createElement('tbody');
-
-    // Create headers based on incident fields
-    const headers = ['id', 'name', 'created_at', 'started_at', 'discarded_at', 'summary', 'customer_impact_summary', 'description', 'current_milestone', 'number', 'priority', 'severity', /* Add other headers as necessary */];
-    const headerRow = document.createElement('tr');
-    headers.forEach(headerText => {
-        const header = document.createElement('th');
-        header.textContent = headerText;
-        headerRow.appendChild(header);
-    });
-    thead.appendChild(headerRow);
-
-    // Create rows
-    incidents.forEach(incident => {
-        const row = document.createElement('tr');
-        headers.forEach(header => {
-            const cell = document.createElement('td');
-            cell.textContent = incident[header] || ''; // Handle null or undefined values
-            row.appendChild(cell);
-        });
-        tbody.appendChild(row);
-    });
-
-    table.appendChild(thead);
-    table.appendChild(tbody);
-    return table;
-}
-
-function createCSVDownloadLink(csvData) {
-    const link = document.createElement('a');
-    link.href = 'data:text/csv;charset=utf-8,' + encodeURIComponent(csvData);
-    link.download = 'incidents_report.csv';
-    link.textContent = 'Download CSV';
-    return link;
 }

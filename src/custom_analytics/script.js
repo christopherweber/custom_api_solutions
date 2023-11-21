408103c005aa42f3d6b6ff2e1749e17f489b5fe1
chrisweber <cweber@firehydrant.io>
2023-11-21 13:39:49 -0500

    complete change of api

--- src/custom_analytics/script.js
+++ src/custom_analytics/script.js
@@ -57,19 +57,56 @@ function displayReportResults(data) {
     const reportResultsElement = document.getElementById('reportResults');
     reportResultsElement.innerHTML = '';
 
-    if (!data || !data.incidents || data.incidents.length === 0) {
+    // Check if the data is in the expected format
+    if (!data || !data.data || data.data.length === 0) {
         reportResultsElement.textContent = 'No data to display.';
         return;
     }
 
-    // Create and populate table
-    const table = createTable(data.incidents);
-    reportResultsElement.appendChild(table);
+    // Assuming data is wrapped in a "data" key as per your example
+    const incidents = data.data;
 
-    // Create CSV download link
-    const csvDownloadLink = createCSVDownloadLink(data.csv);
-    reportResultsElement.appendChild(csvDownloadLink);
+    // Create and populate table
+    const table = createTable(incidents);
+    reportResultsElement.appendChild(table);
 }
+
+function createTable(incidents) {
+    const table = document.createElement('table');
+    const thead = document.createElement('thead');
+    const tbody = document.createElement('tbody');
+
+    // Create headers based on keys of the first incident
+    const headers = Object.keys(incidents[0]);
+    const headerRow = document.createElement('tr');
+    headers.forEach(headerText => {
+        const header = document.createElement('th');
+        header.textContent = headerText;
+        headerRow.appendChild(header);
+    });
+    thead.appendChild(headerRow);
+
+    // Create rows for each incident
+    incidents.forEach(incident => {
+        const row = document.createElement('tr');
+        headers.forEach(header => {
+            const cell = document.createElement('td');
+            if (header in incident) {
+                // Check if the value is an object and convert it to string
+                cell.textContent = typeof incident[header] === 'object' ? JSON.stringify(incident[header]) : incident[header];
+            } else {
+                cell.textContent = '';
+            }
+            row.appendChild(cell);
+        });
+        tbody.appendChild(row);
+    });
+
+    table.appendChild(thead);
+    table.appendChild(tbody);
+    return table;
+}
+
 
 function createTable(incidents) {
     const table = document.createElement('table');

62a4289411fa86a9b84a9d98df2c75c374b3e2b6
chrisweber <cweber@firehydrant.io>
2023-11-22 12:52:46 -0500

    added fields

--- functions/getAnalytics.js
+++ functions/getAnalytics.js
@@ -5,43 +5,35 @@ exports.handler = async function(event) {
   try {
     const { authToken, startDate, endDate } = JSON.parse(event.body);
     const incidentsUrl = `https://api.firehydrant.io/v1/incidents?start_date=${startDate}&end_date=${endDate}`;
-    console.log("API Request URL: " + incidentsUrl);
 
     const response = await axios.get(incidentsUrl, {
       headers: { 'Authorization': authToken }
     });
 
-    // Check and log the entire response to understand its structure
-    console.log("Full API Response:", response.data);
+    const incidents = response.data.data || []; // Adjust based on actual API response structure
 
-    // Check if the incidents data is directly under the data key
-    let incidents;
-    if (Array.isArray(response.data)) {
-      incidents = response.data;
-    } else if (response.data && Array.isArray(response.data.data)) {
-      // If the data is under data.data
-      incidents = response.data.data;
-    } else {
-      throw new Error("Unexpected response format from the FireHydrant API");
-    }
+    const formattedIncidents = incidents.map(incident => {
+      return {
+        id: incident.id,
+        name: incident.name,
+        created_at: incident.created_at,
+        started_at: incident.started_at,
+        severity: incident.severity,
+        priority: incident.priority,
+        tags: incident.tag_list.join(', '),
+        custom_fields: formatCustomFields(incident.custom_fields),
+        opened_by: incident.created_by ? incident.created_by.name : 'N/A',
+        milestones: formatMilestones(incident.milestones),
+        impacts: formatImpacts(incident.impacts)
+      };
+    });
 
-    // Log the first few incidents to avoid large log outputs
-    if (incidents && incidents.length > 0) {
-      console.log("First Incident:", incidents[0]);
-    if (incidents.length > 1) {
-      console.log("Second Incident:", incidents[1]);
-    }
-  }
-
-    console.log("Incidents Data:", incidents);
-
-    // Convert the incidents data to CSV format
-    const fields = ['id', 'name', 'created_at', 'started_at', 'discarded_at', 'summary', 'customer_impact_summary', 'description', 'current_milestone', 'number', 'priority', 'severity'];
-    const csv = parse(incidents, { fields });
+    const fields = ['id', 'name', 'created_at', 'started_at', 'severity', 'priority', 'tags', 'custom_fields', 'opened_by', 'milestones', 'impacts'];
+    const csv = parse(formattedIncidents, { fields });
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ incidents, csv })
+      body: JSON.stringify({ incidents: formattedIncidents, csv })
     };
   } catch (error) {
     console.error('Error:', error);
@@ -52,10 +44,14 @@ exports.handler = async function(event) {
   }
 };
 
+function formatCustomFields(fields) {
+  return fields.map(field => `${field.name}: ${field.value_string || field.value_array.join(', ')}`).join('; ');
+}
 
-// Additional functions (if needed) can be added below
+function formatMilestones(milestones) {
+  return milestones.map(milestone => `${milestone.type} (duration: ${milestone.duration || 'N/A'})`).join('; ');
+}
 
-
-async function fetchIncidentDetails(incidentId, authToken) {
-  // Nothing here yet 
+function formatImpacts(impacts) {
+  return impacts.map(impact => `${impact.type}: ${impact.impact.name} (Condition: ${impact.condition.name})`).join('; ');
 }
--- src/custom_analytics/script.js
+++ src/custom_analytics/script.js
@@ -1,94 +1,93 @@
 document.addEventListener('DOMContentLoaded', () => {
     fetch('../sidebar.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('sidebar-placeholder').innerHTML = data;
-            attachFormSubmitListener();
-        })
-        .catch(error => console.error('Error loading the sidebar:', error));
-});
-
-function attachFormSubmitListener() {
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('sidebar-placeholder').innerHTML = data;
+        attachFormSubmitListener();
+      })
+      .catch(error => console.error('Error loading the sidebar:', error));
+  });
+  
+  function attachFormSubmitListener() {
     const form = document.getElementById('analyticsForm');
     form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        const authToken = document.getElementById('authToken').value;
-        const startDate = document.getElementById('startDate').value;
-        const endDate = document.getElementById('endDate').value;
-        showLoadingMessage();
-        fetchAnalyticsData(authToken, startDate, endDate);
+      event.preventDefault();
+      const authToken = document.getElementById('authToken').value;
+      const startDate = document.getElementById('startDate').value;
+      const endDate = document.getElementById('endDate').value;
+      showLoadingMessage();
+      fetchAnalyticsData(authToken, startDate, endDate);
     });
-}
-
-function fetchAnalyticsData(authToken, startDate, endDate) {
+  }
+  
+  function fetchAnalyticsData(authToken, startDate, endDate) {
     showLoadingMessage();
     fetch('/.netlify/functions/getAnalytics', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ authToken, startDate, endDate })
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ authToken, startDate, endDate })
     })
     .then(response => response.json())
     .then(data => {
-        hideLoadingMessage();
-        displayReportResults(data);
+      hideLoadingMessage();
+      displayReportResults(data);
     })
     .catch(error => {
-        console.error('Error:', error);
-        hideLoadingMessage();
+      console.error('Error:', error);
+      hideLoadingMessage();
     });
-}
-
-function showLoadingMessage() {
+  }
+  
+  function showLoadingMessage() {
     const loadingElement = document.getElementById('loadingMessage');
     loadingElement.textContent = 'Loading... Please wait.';
     loadingElement.style.display = 'block';
-}
-
-function hideLoadingMessage() {
+  }
+  
+  function hideLoadingMessage() {
     document.getElementById('loadingMessage').style.display = 'none';
-}
-
-function displayReportResults(data) {
+  }
+  
+  function displayReportResults(data) {
     const reportResultsElement = document.getElementById('reportResults');
     reportResultsElement.innerHTML = '';
-
-    // Update to correctly reference data.incidents
+    
     if (!data || !Array.isArray(data.incidents) || data.incidents.length === 0) {
-        reportResultsElement.textContent = 'No data to display.';
-        return;
+      reportResultsElement.textContent = 'No data to display.';
+      return;
     }
-
+  
     const table = createTable(data.incidents);
     reportResultsElement.appendChild(table);
-}
-
-
-function createTable(incidents) {
+  }
+  
+  function createTable(incidents) {
     const table = document.createElement('table');
     table.setAttribute('border', '1');
     const thead = document.createElement('thead');
     const tbody = document.createElement('tbody');
-
-    const headers = ['id', 'name', 'created_at', 'started_at', 'discarded_at', 'summary', 'customer_impact_summary', 'description', 'current_milestone', 'number', 'priority', 'severity'];
+  
+    const headers = ['id', 'name', 'created_at', 'started_at', 'severity', 'priority', 'tags', 'custom_fields', 'opened_by', 'milestones', 'impacts'];
     const headerRow = document.createElement('tr');
     headers.forEach(headerText => {
-        const header = document.createElement('th');
-        header.textContent = headerText;
-        headerRow.appendChild(header);
+      const header = document.createElement('th');
+      header.textContent = headerText;
+      headerRow.appendChild(header);
     });
     thead.appendChild(headerRow);
     table.appendChild(thead);
-
+  
     incidents.forEach(incident => {
-        const row = document.createElement('tr');
-        headers.forEach(header => {
-            const cell = document.createElement('td');
-            cell.textContent = incident[header] || '';
-            row.appendChild(cell);
-        });
-        tbody.appendChild(row);
+      const row = document.createElement('tr');
+      headers.forEach(header => {
+        const cell = document.createElement('td');
+        cell.textContent = incident[header] || '';
+        row.appendChild(cell);
+      });
+      tbody.appendChild(row);
     });
-
+  
     table.appendChild(tbody);
     return table;
-}
+  }
+  

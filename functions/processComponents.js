235b923f43d8b2d307f318ff6d27cf4b343565c9
chrisweber <cweber@firehydrant.io>
2023-11-29 19:02:44 -0500

    new page 3

--- functions/processComponents.js
+++ functions/processComponents.js
@@ -107,9 +107,8 @@ async function fetchInfrastructureId(name, authToken) {
             }
 
             // Check if there are more pages to fetch
-            if (infrastructures.length === 0 || !response.data.pagination || !response.data.pagination.next_page) {
+            if (infrastructures.length < 20) {
                 console.log(`No more pages to fetch. Total infrastructures checked: ${totalInfrastructuresProcessed}. Infrastructure with name '${name}' not found.`);
-                console.log(":)")
                 return null;
             }
 
@@ -121,6 +120,7 @@ async function fetchInfrastructureId(name, authToken) {
         throw error;
     }
 }
+
 
 
 

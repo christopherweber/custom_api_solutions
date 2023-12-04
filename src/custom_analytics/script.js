3da1e6b8898044aa9ddb4ef2c7b1722b27f7f428
chrisweber <cweber@firehydrant.io>
2023-12-04 17:09:18 -0500

    I'm not even sure if this will work

--- src/custom_analytics/script.js
+++ src/custom_analytics/script.js
@@ -1,156 +1,83 @@
 document.addEventListener('DOMContentLoaded', () => {
-  attachFormSubmitListener();
+  const form = document.getElementById('analyticsForm');
+  form.addEventListener('submit', handleSubmit);
+
   const addFilterBtn = document.getElementById('addFilter');
-  if (addFilterBtn) {
-      addFilterBtn.addEventListener('click', addFilter);
-  }
+  addFilterBtn.addEventListener('click', addFilter);
+
+  const backToActionsBtn = document.getElementById('backToActions');
+  backToActionsBtn.addEventListener('click', goBack);
 });
 
 function goBack() {
   window.history.back();
 }
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const authToken = document.getElementById('authToken').value;
+  const startDate = document.getElementById('startDate').value;
+  const endDate = document.getElementById('endDate').value;
+  fetchAnalyticsData(authToken, startDate, endDate);
+}
+
+function fetchAnalyticsData(authToken, startDate, endDate) {
+  showLoadingMessage();
+  fetch('/.netlify/functions/getAnalytics', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ authToken, startDate, endDate })
+  })
+  .then(response => response.json())
+  .then(data => {
+    hideLoadingMessage();
+    displayReportResults(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+    hideLoadingMessage();
+  });
+}
+
 function addFilter() {
-  // Assuming you want to add a milestone filter
-  const filtersContainer = document.getElementById('additionalFilters'); // Make sure this container exists in your HTML
+  const filtersContainer = document.getElementById('additionalFilters');
+  filtersContainer.style.display = 'block';
   const newFilter = document.createElement('div');
   newFilter.className = 'form-field';
   newFilter.innerHTML = `
-      <label for="milestoneDropdown">Milestone:</label>
-      <select id="milestoneDropdown">
-          <option value="started">Started</option>
-          <option value="detected">Detected</option>
-          <option value="acknowledged">Acknowledged</option>
-          <option value="investigating">Investigating</option>
-          <option value="identified">Identified</option>
-          <option value="mitigated">Mitigated</option>
-          <option value="resolved">Resolved</option>
-          <option value="postmortem_started">Postmortem Started</option>
-          <option value="postmortem_completed">Postmortem Completed</option>
-      </select>
+    <label for="milestoneDropdown">Milestone:</label>
+    <select id="milestoneDropdown">
+      <option value="started">Started</option>
+      // Add other options as needed
+    </select>
   `;
   filtersContainer.appendChild(newFilter);
 }
 
-
-
-// function toggleMilestoneField() {
-//   const milestoneField = document.getElementById('milestoneField');
-//   const toggleButton = document.getElementById('toggleMilestone');
-//   if (milestoneField.style.display === 'none') {
-//       milestoneField.style.display = 'block';
-//       toggleButton.textContent = '-';
-//   } else {
-//       milestoneField.style.display = 'none';
-//       toggleButton.textContent = '+';
-//   }
-// }
-
-// function addMilestone() {
-//   const select = document.getElementById('milestoneDropdown');
-//   const selectedValue = select.value;
-//   const selectedDiv = document.getElementById('selectedMilestones');
-//   if (Array.from(selectedDiv.children).some(child => child.textContent.includes(selectedValue))) {
-//       alert('This milestone is already selected.');
-//       return;
-//   }
-//   const newMilestone = document.createElement('span');
-//   newMilestone.textContent = selectedValue;
-//   newMilestone.classList.add('selectedMilestone');
-//   selectedDiv.appendChild(newMilestone);
-//   const removeBtn = document.createElement('button');
-//   removeBtn.textContent = 'Remove';
-//   removeBtn.onclick = function() { newMilestone.remove(); };
-//   newMilestone.appendChild(removeBtn);
-// }
-
-function attachFormSubmitListener() {
-  const form = document.getElementById('analyticsForm');
-  form.addEventListener('submit', function(event) {
-      event.preventDefault();
-      const authToken = document.getElementById('authToken').value;
-      const startDate = document.getElementById('startDate').value;
-      const endDate = document.getElementById('endDate').value;
-      showLoadingMessage();
-      fetchAnalyticsData(authToken, startDate, endDate);
-  });
+function showLoadingMessage() {
+  const loadingElement = document.getElementById('loadingMessage');
+  loadingElement.textContent = 'Loading... Please wait.';
+  loadingElement.style.display = 'block';
 }
 
-    let currentPage = 1;
-    const pageSize = 10; // Adjust based on your API's pagination
-    let loadingMoreData = false;
-  
-    function fetchAnalyticsData(authToken, startDate, endDate) {
-      showLoadingMessage();
-      fetch('/.netlify/functions/getAnalytics', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ authToken, startDate, endDate })
-      })
-      .then(response => response.json())
-      .then(data => {
-        hideLoadingMessage();
-        displayReportResults(data);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-        hideLoadingMessage();
-      });
-    }
-    
+function hideLoadingMessage() {
+  const loadingElement = document.getElementById('loadingMessage');
+  loadingElement.style.display = 'none';
+}
 
-  //window.onscroll = function() {
-  //  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight && !loadingMoreData) {
-  //    loadingMoreData = true;
-  //    currentPage += 1;
-  //    fetchAnalyticsData(authToken, startDate, endDate, currentPage);
-// }
-  // };
+function displayReportResults(data) {
+  const reportResultsElement = document.getElementById('reportResults');
+  if (!data || !data.length) {
+    reportResultsElement.textContent = 'No data to display.';
+    return;
+  }
 
-  function showLoadingMessage() {
-    const loadingElement = document.getElementById('loadingMessage');
-    loadingElement.textContent = 'Loading... Please wait.';
-    loadingElement.style.display = 'block';
-  }
-  
-  function hideLoadingMessage() {
-    document.getElementById('loadingMessage').style.display = 'none';
-  }
-  
-  let csvData = ''; // Global variable to store CSV data
+  const table = createTable(data);
+  reportResultsElement.appendChild(table);
 
-  function displayReportResults(data) {
-      const reportResultsElement = document.getElementById('reportResults');
-      const downloadCsvButton = document.getElementById('downloadCsv');
-      reportResultsElement.innerHTML = '';
-  
-      // Check if data is empty or undefined
-      if (!data || !Array.isArray(data.incidents) || data.incidents.length === 0) {
-          reportResultsElement.textContent = 'No data to display.';
-          downloadCsvButton.style.display = 'none'; // Hide the CSV download button
-          return;
-      }
-  
-      // Data is present, show the CSV download button
-      downloadCsvButton.style.display = 'block';
-  
-      // Store the CSV data for download
-      if (data && data.csv) {
-          csvData = data.csv;
-      }
-  
-      // Create and display the table with incident data
-      const table = createTable(data.incidents);
-      reportResultsElement.appendChild(table);
-      const exportButton = document.getElementById('exportCsv');
-      if (data && data.length > 0) {
-          // Show the export button if data is present
-          exportButton.style.display = 'block';
-      } else {
-          // Hide the export button if no data is present
-          exportButton.style.display = 'none';
-      }
-  }
+  const downloadCsvButton = document.getElementById('downloadCsv');
+  downloadCsvButton.style.display = 'block';
+}
   
 function createTable(incidents) {
     const table = document.createElement('table');
@@ -196,30 +123,6 @@ function createTable(incidents) {
     table.appendChild(tbody);
     return table;
 }
-
-function addMilestone() {
-    const select = document.getElementById('milestoneDropdown');
-    const selectedValue = select.value;
-    const selectedDiv = document.getElementById('selectedMilestones');
-  
-    // Check if the milestone is already selected
-    if (Array.from(selectedDiv.children).some(child => child.textContent.includes(selectedValue))) {
-      alert('This milestone is already selected.');
-      return;
-    }
-  
-    // Create a new span element to display the selected milestone
-    const newMilestone = document.createElement('span');
-    newMilestone.textContent = selectedValue;
-    newMilestone.classList.add('selectedMilestone');
-    selectedDiv.appendChild(newMilestone);
-  
-    // Optionally, add a remove button to each milestone
-    const removeBtn = document.createElement('button');
-    removeBtn.textContent = 'Remove';
-    removeBtn.onclick = function() { newMilestone.remove(); };
-    newMilestone.appendChild(removeBtn);
-  }
   
   document.getElementById('downloadCsv').addEventListener('click', function() {
     if (!csvData) {
